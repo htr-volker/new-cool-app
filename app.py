6b9ec1ff9b5a3c99cd7955f60cda1ede6c6516d3
St00par <stewart.mackenzie-nwse@outlook.com>
2021-09-27 13:58:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Stewart",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "haggis,
 ]
 
 @app.route('/')
