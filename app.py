fcc4143fc78a7e512ca9ef85e3b04ad4d2026655
graydb <86656673+graydb@users.noreply.github.com>
2021-09-27 14:09:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Dougie",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "chinese",
 ]
 
 @app.route('/')
