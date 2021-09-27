b9b7b1a452740ded12d610120025d8803437d005
Lesley Ferrie <86656682+lesleyengineer@users.noreply.github.com>
2021-09-27 14:12:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Lesley",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "sausages",
 ]
 
 @app.route('/')
