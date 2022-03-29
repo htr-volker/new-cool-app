7b12679cd74f69dec01cbe2286cf37a153bd140f
Rhys <rhysconnor604@gmail.com>
2022-03-29 15:39:43 +0100

    Fixing issue

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Rhys",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "Sushi",
 ]
 
 @app.route('/')
