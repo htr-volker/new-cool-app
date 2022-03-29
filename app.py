0c88d4d0120dec0b7ed794b068c1569de805d3ce
Jonas Gedeshi <jonas.gedeshi@gmail.com>
2022-03-29 16:48:43 +0200

    add own name and food

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Jonas",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "bürek",
 ]
 
 @app.route('/')
