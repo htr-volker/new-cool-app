5d56ce431f9cec2525ffcb9c7eee5454b043a1ee
HarryRitchie98 <harry.ritchie@atos.net>
2022-03-29 15:41:07 +0100

    app.py food and name change

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 # Add your name in this list!
 trainees = [
-    "Harry",
+    "Harry mk2",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
-    "pizza",
+    "curry",
 ]
 
 @app.route('/')
