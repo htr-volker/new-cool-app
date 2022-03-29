08446b0d4d2b77bc69b152343f5dbf6e64450263
Joe van Kollenburg <joevankollenburg@hotmail.com>
2022-03-29 16:49:05 +0200

    Added Joe and his favorite food

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Joe",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "ramen",
 ]
 
 @app.route('/')
