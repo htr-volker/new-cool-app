44398053e6cfc9c30c7d6b6c6475fda82ea6a529
Van Isacker <andrea.vanisacker@atos.net>
2022-03-29 17:02:44 +0200

    added fave food

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 # Add your name in this list!
 trainees = [
-    "Harry",
+    "Harry", "Andrea",
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
-    "pizza",
+    "pizza", "sushi",
 ]
 
 @app.route('/')
