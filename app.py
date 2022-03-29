5729f2186e9b0c317da99180f06050a9dd07a8a1
Aisha <aisha.bi@outlook.com>
2022-03-29 15:33:33 +0100

    Aisha

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Aisha"
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "cake"
 ]
 
 @app.route('/')
